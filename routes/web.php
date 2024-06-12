30877f6dc17df41de47101575e96505f8c7654bb
Eduardo Pillado <eduardompilladoo5@gmail.com>
2024-06-12 11:38:57 -0700

    Prueba 4

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Usuario_controller;
 
 Route::get('/', function () {
     return view('inicio');
 });
+
+Route::get('/login', function () {
+    $PK_USUARIO = session('pk_usuario');
+    if ($PK_USUARIO) {
+        return redirect()->back()->with('warning', 'Ya has iniciado sesión');
+    } else {
+        return view('login');
+    }
+})->name('login');
+
+// Usuario ----------------------------------------------------------------------------------------------------
+
+Route::get('/registro', function () {
+    $PK_USUARIO = session('pk_usuario');
+    if ($PK_USUARIO) {
+        return redirect()->back()->with('warning', 'Cierra sesión para acceder');
+    } else {
+        return view('registro');
+    }
+})->name('registro');
+
+Route::post('/iniciandoSesión', [Usuario_controller::class, 'login'])->name('usuario.login');
+Route::get('/cerrandoSesión', [Usuario_controller::class, 'logout'])->name('usuario.logout');
+
+Route::post('/registrando', [Usuario_controller::class, 'insertar'])->name('usuario.insertar');
+
+
+// ------------------------------------------------------------------------------------------------------------
